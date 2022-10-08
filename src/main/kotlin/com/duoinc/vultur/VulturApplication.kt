4f2bd7d0c9eefb613dfe742bdd24bb29b9a372a7
Denys Maiolli <denysmaiolli@hotmail.com>
2022-10-07 21:43:24 -0300

    project setup

---
+++ src/main/kotlin/com/duoinc/vultur/VulturApplication.kt
@@ -0,0 +1,11 @@
+package com.duoinc.vultur
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VulturApplication
+
+fun main(args: Array<String>) {
+	runApplication<VulturApplication>(*args)
+}
---
+++ src/test/kotlin/com/duoinc/vultur/VulturApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.duoinc.vultur
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VulturApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
