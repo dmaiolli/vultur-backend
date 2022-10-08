a2742c4a8573d8a8bb4c88189076c5f848493432
Denys Maiolli <denysmaiolli@hotmail.com>
2022-10-08 11:55:47 -0300

    feat: Get profile html from instagram

--- src/main/kotlin/com/duoinc/vultur/worker/SearchKeywordsOnInstagramSchedule.kt
+++ src/main/kotlin/com/duoinc/vultur/worker/SearchKeywordsOnInstagramSchedule.kt
@@ -1,15 +1,18 @@
 package com.duoinc.vultur.worker
 
+import com.duoinc.vultur.domain.model.Profile
+import com.duoinc.vultur.service.InstagramService
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.util.Date
 
 @Component
-class SearchKeywordsOnInstagramSchedule {
+class SearchKeywordsOnInstagramSchedule(private val instagramService: InstagramService) {
 
     @Scheduled(cron = "0/5 * * * * ?")
     fun everyThreeSeconds() {
-        println("Periodic task: " + Date())
+        val profile = Profile("1", "1", "dmaiolli", listOf(""))
+        println(instagramService.getProfileDescription(profile))
     }
 
 }
